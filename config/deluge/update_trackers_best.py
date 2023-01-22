3e64e756e7fbb8fbfcbafe2ee050b7b0913546de
David O Neill <dmz.oneill@gmail.com>
2023-01-22 16:46:55 +0000

    Updates

--- config/deluge/update_trackers_best.py
+++ config/deluge/update_trackers_best.py
@@ -1,4 +1,5 @@
 import urllib.request
+import requests
 import os
 import sys
 import platform
@@ -8,7 +9,29 @@ from pprint import pprint
 
 dont_touch_torrents = ['localhost.stackoverflow.tech', 'ssl.empirehost.me', 'routing.bgp.technology', 'stackoverflow.tech', 'empirehost.me']
 best_trackers_ips = "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best_ip.txt"
+best_trackers_newtrackton = "https://newtrackon.com/api/stable"
 trackers = ""
+newtrackton = ""
+
+headers = {
+    'authority': 'newtrackon.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'cache-control': 'no-cache',
+    'dnt': '1',
+    'pragma': 'no-cache',
+    'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+}
+
+newtrackton = requests.get('https://newtrackon.com/api/stable', headers=headers).text
 
 with urllib.request.urlopen(best_trackers_ips) as f:
     trackers = f.read().decode('utf-8')
@@ -26,7 +49,7 @@ for torrent in state.torrents:
         torrent_tracker_urls += trk['url'] + ","
 
     if not any(x in torrent_tracker_urls for x in dont_touch_torrents):
-        for tracker in trackers.splitlines():
+        for tracker in newtrackton.splitlines():
             if tracker.strip() == "":
                 continue
             if tracker not in torrent.trackers:
